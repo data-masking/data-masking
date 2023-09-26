c8ba9350880a5556a701686df512cccc266459e2
HS <ihongshu@dingtalk.com>
2023-09-26 11:48:15 +0800

    文档修改

--- datamasking-core/src/main/java/com/github/hongshu/datamasking/annotation/DataMasking.java
+++ datamasking-core/src/main/java/com/github/hongshu/datamasking/annotation/DataMasking.java
@@ -17,18 +17,21 @@ public @interface DataMasking {
     /**
      * desc  脱敏开关，可以控制类、变量
      * 2023/8/11 9:19
+     * @return st
      */
     boolean enabled() default true;
 
 
     /**
      * 开始位置（包含）
+     * @return st
      */
     int startInclude() default 1;
 
     /**
      * 结束位置（不包含）
      * 默认字符串长度
+     * @return st
      */
     int endExclude() default Integer.MAX_VALUE;
 
@@ -36,22 +39,25 @@ public @interface DataMasking {
      * 被替换的字符
      * 字符串长度为1时，等量替换，如： abcedfg : a******
      * 长度大于1时，非等量替换字字符串，如： masking=**  结果 abcedfg : a**
+     * @return st
      */
     String masking() default "*";
 
     /**
      * 数据脱敏策略，默认直接替换
-     *
+     * @return st
      */
     DesensitizedUtil.DesensitizedType strategy() default DesensitizedUtil.DesensitizedType.FIRST_MASK;
 
     /**
      * 当父类上有脱敏标识时，父类下的属性是否脱敏，默认脱敏
+     * @return st
      */
     boolean inherit() default true;
 
     /**
      * 脱敏百分比
+     * @return st
      */
     float maskingPercent() default 0.0F;
 
